36d1f592ccbe7057eaf2fb4a9307041258d9f9e0
Vinnie <novopashinasv10@gmail.com>
2023-09-06 20:15:01 +0500

    WebAdv

--- WebAdv/WebAdv/app_advertisements/models.py
+++ WebAdv/WebAdv/app_advertisements/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class Advertisement(models.Model):
     title = models.CharField("заголовок", max_length=128)
@@ -11,7 +13,13 @@ class Advertisement(models.Model):
     auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('изображения', upload_to='advertisements/')
 
+    @admin.display(description='фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html('<img src="{url}" style="max-width: 80px; max-heigth: 80px;">', url=self.image.url)
 
     @admin.display(description='дата последнего обновления')
     def updated_date(self):
@@ -34,3 +42,4 @@ class Advertisement(models.Model):
     
     class Meta:
         db_table = "advertisements"
+
